a789accec06975649df13a99aecb6a0e42f26916
martino vivien <vivien.martino@hotmail.fr>
2024-11-25 17:01:02 +0100

    Ajout des validations

--- app/models/culture.rb
+++ app/models/culture.rb
@@ -1,4 +1,8 @@
 class Culture < ApplicationRecord
   belongs_to :user
   has_many :tasks
+  validates :name, presence: true
+  validates :plantation_date, presence: true
+
+  CULTURE_STATUS = []
 end
