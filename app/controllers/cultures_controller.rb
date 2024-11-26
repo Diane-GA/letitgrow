9d72647f6d82841205240a6d7a5e8b2bc6386da7
Diane Atticus <diane.atticus@laposte.net>
2024-11-26 12:57:35 +0100

    ajout methode index et view

--- app/controllers/cultures_controller.rb
+++ app/controllers/cultures_controller.rb
@@ -1,2 +1,8 @@
 class CulturesController < ApplicationController
+  
+  def index
+    # on récupère toutes les cultures propre au user connecté
+    @cultures = Culture.where(user: current_user)
+  end
+
 end
