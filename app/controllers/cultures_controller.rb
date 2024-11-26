1551c3cbe500f3ccb3450ee6e5e8a37857f0fb7b
martino vivien <vivien.martino@hotmail.fr>
2024-11-26 15:33:35 +0100

    show d'une culture

--- app/controllers/cultures_controller.rb
+++ app/controllers/cultures_controller.rb
@@ -1,8 +1,11 @@
 class CulturesController < ApplicationController
-  
+
   def index
     # on récupère toutes les cultures propre au user connecté
     @cultures = Culture.where(user: current_user)
   end
 
+  def show
+    @culture = Culture.find(params[:id])
+  end
 end
