695e15e6e5efcbd66f5d72fcbd9cd1d7062af65f
Diane Atticus <diane.atticus@laposte.net>
2024-11-26 14:02:44 +0100

    ajout boutons sur index

--- app/controllers/cultures_controller.rb
+++ app/controllers/cultures_controller.rb
@@ -1,8 +1,14 @@
 class CulturesController < ApplicationController
-  
+
   def index
     # on récupère toutes les cultures propre au user connecté
     @cultures = Culture.where(user: current_user)
   end
 
+  def new
+  end
+
+  def create
+  end
+  
 end
