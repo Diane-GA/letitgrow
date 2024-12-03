3859e3b2d2faa52d0c78adc017e8200806122d41
Clement lemarie <big_m@live.fr>
2024-12-03 11:24:15 +0100

    formatting

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,35 +16,34 @@ class Task < ApplicationRecord
 
   def category_picto()
     if self.category == "Planter"
-      return 'fi fi-rr-shovel'
+      return "fi fi-rr-shovel"
     elsif self.category == "Arroser"
-      return 'fi fi-rs-raindrops'
+      return "fi fi-rs-raindrops"
     elsif self.category == "Tailler"
-      return 'fi fi-rs-scissors'
+      return "fi fi-rs-scissors"
     elsif self.category == "Encourager"
-      return 'fi fi-tr-hand-holding-seeding'
+      return "fi fi-tr-hand-holding-seeding"
     elsif self.category == "Protéger"
-      return 'fi fi-bs-shield'
+      return "fi fi-bs-shield"
     elsif self.category == "Récolter"
-      return 'fi fi-rs-hand-paper'
+      return "fi fi-rs-hand-paper"
     elsif self.category == "Arracher"
-      return 'fi fi-rr-hourglass-end'
+      return "fi fi-rr-hourglass-end"
     elsif self.category == "Custom"
-      return 'fi fi-rc-settings'
+      return "fi fi-rc-settings"
     end
   end
 
   # méthode qui attribue une date d'action en fonction du délai
   # propre à la tâche et de la plantation_date de la culture
   def set_date
-      puts "Mise à jour de la date"
-      start_date = self.culture.plantation_date
-      self.date = start_date + self.delay unless category == "Custom"
+    puts "Mise à jour de la date"
+    start_date = self.culture.plantation_date
+    self.date = start_date + self.delay unless category == "Custom"
   end
 
   # méthode pour attribuer par défaut la valeur false à "done"
   def set_default_done
     self.update(done: false)
   end
-
 end
